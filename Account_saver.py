647ae3c5f8f2132a690453c0f8d687c0157c9c5a
patsuli <39671914+patsuli@users.noreply.github.com>
2018-10-13 16:10:59 +0300

    Create Account_saver.py

---
+++ Account_saver.py
@@ -0,0 +1,13 @@
+person = input('Valitse käyttäjätunnus::')
+print(person, 'on jo varattu')
+toka = input('Valitse toinen käyttäjätunnus:')
+print(toka, 'on käyttäjätunnuksesi.')
+salasana = input('Valitse salasana:')
+
+#poistaa tekstitiedostosta kaiken ja kirjoittaa sinne Usernamen ja Salasanan yhteen. Miten tehdä selkeämpi txt file?
+
+text_file = open("salasanat.txt", "w")
+text_file.write(toka)
+text_file.write(salasana)
+
+text_file.close()
